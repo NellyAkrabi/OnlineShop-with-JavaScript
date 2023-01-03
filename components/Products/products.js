760301dea9663289fdf7f521461fd95f8562296f
Nelly Akrabi <akrabinelly@mail.ru>
2023-01-03 21:25:10 +0100

    Root_Products linked to products.js

--- components/Products/products.js
+++ components/Products/products.js
@@ -1,15 +1,14 @@
 
 
 class Products {
-
-
     render() {
         let htmlCatalog = '';
         CATALOG.forEach(({id, name, img, price}) => {
+
             htmlCatalog += `
                 <li>
                     <span>${name}</span>
-                    <img scr="${img}" />
+                    <img src="${img}" />
                     <span>${price}</span>
                     <button>Add to back</button>
                 </li>
@@ -19,9 +18,10 @@ class Products {
             <ul>
                 ${htmlCatalog}
             </ul>        
-        `
-    }
-}
+        `;
+        ROOT_PRODUCTS.innerHTML= html;
+    };
+};
 
 const productsPage = new Products();
 productsPage.render()
