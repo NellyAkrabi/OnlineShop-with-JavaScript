891b69a0872b6c658ac2e55befdd3aaf8a81dae2
Nelly Akrabi <akrabinelly@mail.ru>
2023-01-04 20:55:19 +0100

    created local server and API

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function render() {
-    
+
   productsPage.render()
 
   const productsStore = localStorageUtil.getProducts();
   headerPage.render(productsStore.length);
 
-}
-render();
+};
+
+
+let CATALOG = [];
+
+
+// API ->   fetch ('https://api.npoint.io/93ce953c6ce93c5214c7')
+fetch ('server/catalog.json')
+.then(res => res.json())
+.then(body => {
+    CATALOG = body;
+    render();
+})
+.catch (error =>{
+    console.log('error');
+})
