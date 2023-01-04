64ab186cfa6a83d3fa1ad34c81a8c1a9711bfb45
Nelly Akrabi <akrabinelly@mail.ru>
2023-01-04 11:00:15 +0100

    products gestyled

--- components/Products/products.js
+++ components/Products/products.js
@@ -6,16 +6,21 @@ class Products {
         CATALOG.forEach(({id, name, img, price}) => {
 
             htmlCatalog += `
-                <li>
-                    <span>${name}</span>
-                    <img src="${img}" />
-                    <span>${price}</span>
-                    <button>Add to back</button>
+                <li class = "products-element">
+                    <span class= "products-element__name">${name}</span>
+                    <img class= "products-element__img" src="${img}" />
+                    <span class= "products-element__price">
+                        ⚡️ ${price.toLocaleString()} €
+                     </span>
+                    <button class= "products-element__btn">Add to back</button>
                 </li>
             `;
         });
+
+
+
         const html = `
-            <ul>
+            <ul class="products-container">
                 ${htmlCatalog}
             </ul>        
         `;
